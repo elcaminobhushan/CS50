0e1d2c9823e3df05b08800871de53e275364218b
elcaminobhushan <53351455+elcaminobhushan@users.noreply.github.com>
2019-09-05 18:59:02 +0530

    Create mario.cpp

---
+++ mario.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>0 && n<24)
+    {
+    for(int i=1 ; i<=n ; i++)
+    { 
+        for(int j=0 ; j<(n-i) ; j++)
+        {
+            cout<<" ";
+            
+        }
+    for(int k=1;k<=i;k++)
+    {cout<<"*";}
+    cout<<"\n";
+    }
+    }
+    else
+    {
+        cout<<"asdasd";
+    }
+
+}
