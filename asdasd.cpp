e8b9861221b1b2fb2e3f77ac933534ea79901ccc
elcaminobhushan <53351455+elcaminobhushan@users.noreply.github.com>
2019-09-05 21:43:48 +0530

    Create asdasd.cpp

---
+++ asdasd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>0 && n<24)
+    {
+    for(int i=1 ; i<=n ; i++)
+    { 
+        for(int j=0 ; j<(n-i) ; j++)
+        {
+            cout<<" ";
+            
+        }
+    for(int k=1;k<=i;k++)
+    {cout<<"*";}
+    cout<<" ";
+    for(int k=1;k<=i;k++)
+    {cout<<"*";}
+    cout<<"\n";
+    }
+    }
+    else
+    {
+        cout<<"INPUT VALUE BETWEEN 1 to 23";
+    }
+
+}
